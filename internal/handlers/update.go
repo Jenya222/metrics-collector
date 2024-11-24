f0c51eac38e6b1f5e561f59db0def849cf1450d0
evgeniy <e.bespalov@galaksion.com>
2024-11-24 13:43:58 +0300

    refactoring

--- internal/handlers/update.go
+++ internal/handlers/update.go
@@ -19,10 +19,6 @@ type metricTypes struct {
 
 type metrics map[string]metricTypes
 
-var supportedMetrics = metrics{
-	"someMetric": {},
-}
-
 var cacheMetrics *MemStorage
 
 func NewUpdateHandler() *UpdateHandler {
@@ -48,33 +44,33 @@ func (h UpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
-	if _, ok := supportedMetrics[path[1]]; !ok {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	m := h.getMemStorage()
-	i, err := strconv.Atoi(path[2])
+	metricsType, metricsName := path[0], path[1]
+	metricsValue, err := strconv.Atoi(path[2])
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
-	switch path[0] {
+	memStorage := h.getMemStorage()
+	switch metricsType {
 	case "counter":
-		if v, ok := m.metrics[path[1]]; ok {
-			v.Counter += int64(i)
-			m.metrics[path[1]] = v
+		if v, ok := memStorage.metrics[metricsName]; ok {
+			v.Counter += int64(metricsValue)
+			memStorage.metrics[metricsName] = v
 		} else {
-			m.metrics[path[1]] = metricTypes{Counter: int64(i), Gauge: 0}
+			memStorage.metrics[metricsName] = metricTypes{Counter: int64(metricsValue), Gauge: 0}
 		}
 	case "gauge":
-		if v, ok := m.metrics[path[1]]; ok {
-			v.Gauge = float64(i)
-			m.metrics[path[1]] = v
+		if v, ok := memStorage.metrics[metricsName]; ok {
+			v.Gauge = float64(metricsValue)
+			memStorage.metrics[metricsName] = v
 		} else {
-			m.metrics[path[1]] = metricTypes{Counter: 0, Gauge: float64(i)}
+			memStorage.metrics[metricsName] = metricTypes{Counter: 0, Gauge: float64(metricsValue)}
 		}
+	default:
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
